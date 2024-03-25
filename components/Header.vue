72bf5a881da65bdc88ec977edf4c1d3f7b037cc8
Melanienurazizah <143985254+Melanienurazizah@users.noreply.github.com>
2024-03-25 14:49:14 +0700

    Add files via upload

--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,13 @@
 <template>
-    <div class="row p-3 bg-primary text-white">
+    <div class="row p-3 bg-Primary text-white">
         <div class="col-lg-1">
-            <img src="~/assets/img/LOGO-SMK.png" alt="logo SMKN 4">
+            <img src="~/assets/img/LOGO-SMK4.png" alt="logo smkn4" height="100px" width="100px">
         </div>
-
         <div class="col-lg-11">
-            <h1>perpus digital</h1>
+            <h1>Perpustakaan Digital</h1>
             <address>
                 SMKN 4 Tasikmalaya
-                Jl.Depok, Sukamenak, Purbaratu
+                JL.Depok, Sukamanah, purbaratu
             </address>
         </div>
     </div>
